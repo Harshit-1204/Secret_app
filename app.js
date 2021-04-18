a13caf6f86aad2a320261bd5abe5152d262fabf0
Harshit <harshitboghara444@gmail.com>
2021-04-18 16:48:44 +0530

    Level-3 Hash function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 const app = express()
 
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:["password"]})
+
 
 const User = mongoose.model("User" , userSchema)
 
@@ -43,7 +43,7 @@ app.get("/register",function(req,res){
 app.post("/register" , function(req,res){
     const user = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     })
     user.save(function(err){
         if(!err){
@@ -56,7 +56,7 @@ app.post("/register" , function(req,res){
 app.post("/login",function(req,res){
     User.findOne({email:req.body.username},function(err,foundUser){
         if(!err){
-            if(foundUser.password === req.body.password){
+            if(foundUser.password === md5(req.body.password)){
                 res.render("secrets")
             }else{
                 console.log("No such user found")
